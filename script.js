2d717ddedc27863cbdac35a827c14862c2fdef7d
Wei He <williehewei@gmail.com>
2026-03-19 10:32:08 -0600

    Add geolocation to display user position on map

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-// Create map centered at Calgary
-var map = L.map('map').setView([51.0447, -114.0719], 12);
+// Create map (temporary center)
+var map = L.map('map').setView([0, 0], 2);
 
 // Add OpenStreetMap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap'
 }).addTo(map);
+
+// Get user location
+if (navigator.geolocation) {
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+
+        // Zoom to user location
+        map.setView([lat, lon], 15);
+
+        // Add marker
+        L.marker([lat, lon])
+            .addTo(map)
+            .bindPopup("You are here")
+            .openPopup();
+
+    }, function() {
+        alert("Location access denied.");
+    });
+
+} else {
+    alert("Geolocation not supported.");
+}
